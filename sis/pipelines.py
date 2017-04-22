c250f9a61ef7a5c01e2ae53af1a70f86f2b3489d
fengyichui <fengyichui@gmail.com>
2017-04-23 01:57:10 +0800

    more stable

--- sis/pipelines.py
+++ sis/pipelines.py
@@ -3,24 +3,38 @@
 import os
 import urllib
 import socket
+import re
+import time
 from sis import settings
- 
-DIR = './save/'
+
+DIR = './save/' + time.strftime('%Y-%m-%d %H-%M-%S',time.localtime(time.time())) + '/'
+
+socket.setdefaulttimeout(30)
 
 class SisPipeline(object):
- 
+
+    def trim_filename(self, filename):
+        return re.sub(r'[\/\\\:\*\?\"\<\>\|]', '-', filename)
+
     def process_item(self, item, spider):
         if not os.path.exists(DIR):
             os.makedirs(DIR)
-        dir = DIR + item['album']['title']
+
+        dir = DIR + self.trim_filename(item['album']['group']) + '/' + self.trim_filename(item['album']['title'])
         if not os.path.exists(dir):
             os.makedirs(dir)
-        socket.setdefaulttimeout(30)
-        urllib.request.urlretrieve(item['album']['torrent_url'],
-                "{}/{}".format(dir,item['album']['torrent_name']))
+
+        try:
+            urllib.request.urlretrieve(item['album']['torrent_url'], "{}/{}".format(dir,item['album']['torrent_name']))
+        except Exception as e:
+            print("Exception: {} ({})".format(str(e), item['album']['torrent_url']))
+
         for image_url in item['album']['image']:
             list_name = image_url.split('/')
             file_name = list_name[len(list_name)-1]
-            socket.setdefaulttimeout(30)
-            urllib.request.urlretrieve(image_url, '{}/{}'.format(dir,file_name))
+            try:
+                urllib.request.urlretrieve(image_url, '{}/{}'.format(dir,file_name))
+            except Exception as e:
+                print("Exception: {} ({})".format(str(e), image_url))
+
         return item
--- sis/spiders/sisSpider.py
+++ sis/spiders/sisSpider.py
@@ -6,40 +6,51 @@ import re
 from scrapy.crawler import CrawlerProcess
 
 WEBSITE = 'http://68.168.16.149/'
-SEARCH = r''
-PAGES = 2
+SEARCH = r'合集'
+PAGES = 10
 
 class sisSpider(scrapy.Spider):
     name = 'sis'
     allowed_domains = []
-    start_urls = [WEBSITE + "forum/forum-58-1.html"]
-    ipage = 0
+    start_urls = [WEBSITE + 'forum/forum-426-1.html',
+                  WEBSITE + 'forum/forum-143-1.html',
+                  WEBSITE + 'forum/forum-230-1.html',
+                  WEBSITE + 'forum/forum-25-1.html',
+                  WEBSITE + 'forum/forum-58-1.html']
+
 
     def parse(self, response):
         titles = response.xpath('//table/tbody/tr/th[@class="new" or @class="common"]/span[@id]/a')
+        page = response.xpath('//div[@class="pages"]/strong//text()').extract()
+        page = int(page[0]) if page else 0
+
         for title in titles:
             text = title.xpath("text()").extract()[0]
-#            print(text)
             match = re.search(SEARCH, text, re.I)
             if match:
+                print("√ [page-{}] {}".format(page, text))
                 href = title.xpath("@href").extract()[0]
-#                print(text)
                 yield scrapy.Request(WEBSITE + "forum/" + href, callback=self.parse_album)
-        self.ipage += 1
-        if self.ipage < PAGES:
-            # Next page
-            new_url= response.xpath('//a[@class="next"]//@href').extract()[0]
+
+        if page < PAGES:
+            new_url= response.xpath('//div[@class="pages"]/a[@class="next"]//@href').extract()[0]
             if new_url:
                 yield scrapy.Request(WEBSITE + "forum/" + new_url, callback=self.parse)
 
+
     def parse_album(self, response):
-        item = SisItem()
-        attach = response.xpath('//dl[@class="t_attachlist"]/dt/a[2]')
+        group = response.xpath('//div[@id="nav"]/a[2]/text()').extract()[0]
         title = response.xpath('//div[@class="postmessage defaultpost"]/h2/text()').extract()[0]
-        image = response.xpath('//div[@class="t_msgfont"]//img/@src').extract()
+        images = response.xpath('//div[@class="t_msgfont"]//img/@src').extract()
+        attach = response.xpath('//dl[@class="t_attachlist"]/dt/a[2]')
         torrent_url = WEBSITE + 'forum/' + attach.xpath('@href').extract()[0]
         torrent_name = attach.xpath('text()').extract()[0]
-        album = dict(title=title, torrent_url=torrent_url, torrent_name=torrent_name, image=image)
-        item['album'] = album
-#        print(album)
+
+        image = []
+        for i in images:
+            if not re.search(r'\.gif$', i) and re.search(r'^http', i):
+                image.append(i)
+
+        item = SisItem()
+        item['album'] = dict(group=group, title=title, torrent_url=torrent_url, torrent_name=torrent_name, image=image)
         yield item
